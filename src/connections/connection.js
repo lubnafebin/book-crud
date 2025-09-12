2ce3eaac9f08a2a9d3faedb9c169980b4fb96fb3
Lubna Febin <lubnafebin13@gmail.com>
2025-09-12 16:28:16 +0530

    updated database url

--- src/connections/connection.js
+++ src/connections/connection.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const connect = async () => {
-    await mongoose.connect('mongodb://localhost:27017/minnu', {
-
-    }).then(() => {
-        console.log('Connected to MongoDB');
-    }).catch(err => {
-        console.log('Failed to connect to MongoDB ');
+  await mongoose
+    .connect(process.env.MONGO_URI, {})
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.log("Failed to connect to MongoDB ");
     });
-}
+};
 module.exports = connect;
