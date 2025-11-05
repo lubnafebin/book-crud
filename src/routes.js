4519425f44974e390f793697d023e05858b257bd
Lubna Febin <lubnafebin13@gmail.com>
2025-11-05 19:56:10 +0530

    updated routes by adding new route for delete operation

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,42 @@
-const express = require('express');
-const Book = require('./schema');
+const express = require("express");
+const Book = require("./schema");
 const router = express.Router();
 
-router.post('/book', async (req, res) => {
-    try {
-        const book = await Book.create(req.body);
-        console.log(book);
-        await book.save();
-        res.status(201).json(book);
-    } catch (error) {
-        res.status(404).send(error);
+router.post("/book", async (req, res) => {
+  try {
+    const book = await Book.create(req.body);
+    console.log(book);
+    await book.save();
+    res.status(201).json(book);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+router.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(201).send(books);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.delete("/:bookId/delete", async (req, res) => {
+  try {
+    const bookId = req.params.bookId;
+    const books = await Book.deleteOne({ _id: bookId });
+    if (books.acknowledged && books.deletedCount == 1) {
+      res
+        .status(201)
+        .json({ success: true, message: "Deleted Successfully", data: books });
+    } else {
+      res.status(404).json({ success: false, message: "Book not found" });
     }
-})
-router.get('/books', async (req, res) => {
-    try {
-        const books = await Book.find();
-        res.status(201).send(books);
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Something Went Wrong" });
+  }
+});
 
 
 
