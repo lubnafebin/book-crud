c3d478529694a8ef57f16d5645ada3abeac01ec2
Lubna Febin <lubnafebin13@gmail.com>
2025-11-05 20:16:24 +0530

    added functionality for update operation and declared route for it

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,10 @@ const router = express.Router();
 router.post("/book", async (req, res) => {
   try {
     const book = await Book.create(req.body);
-    console.log(book);
     await book.save();
-    res.status(201).json(book);
+    res
+      .status(201)
+      .json({ success: true, message: "created successfully", data: book });
   } catch (error) {
     res.status(404).send(error);
   }
@@ -38,6 +39,33 @@ router.delete("/:bookId/delete", async (req, res) => {
   }
 });
 
-
+router.put("/:bookId/update", async (req, res) => {
+  try {
+    const bookId = req.params.bookId;
+    if (
+      req.body?.title ||
+      req.body?.author ||
+      req.body?.publishedYear ||
+      req.body?.genre
+    ) {
+      const books = await Book.updateOne({ _id: bookId }, { $set: req.body });
+      if (books.acknowledged && books.modifiedCount != 0) {
+        res.status(201).json({
+          success: true,
+          message: "Updated successfully",
+          data: books,
+        });
+      } else {
+        res
+          .status(400)
+          .json({ success: false, message: "Not Updated", data: books });
+      }
+    } else {
+      res.status(404).json({ success: false, message: "req body not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: "something went wrong" });
+  }
+});
 
 module.exports = router;
